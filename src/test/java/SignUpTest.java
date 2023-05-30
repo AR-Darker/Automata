1660facf0aa4deebb910fe00689c811b8ac2a687
gk <ishkredov2@gmail.com>
2023-05-30 21:24:35 +0300

    Commit

---
+++ src/test/java/SignUpTest.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.sql.Driver;
+
+public class SignUpTest {
+
+
+    @Test
+    public void zipCodeShouldAccept5Digits(){
+        //1. Перейти на https://www.sharelane.com/cgi-bin/register.py
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        //2. В поле Зип-код ввести - 12345
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys("12345");
+        //3. Нажать кнопку continue
+        WebElement continueButton = driver.findElement(By.cssSelector("[value=Continue]"));
+        continueButton.click();
+        //4. Убедиться что мы перешли на старницу ввода данных пользователя
+        WebElement registerButton = driver.findElement((By.cssSelector("[value=Register]")));
+        boolean isDisplayed = registerButton.isDisplayed();
+        Assert.assertTrue(isDisplayed, "Нужная страница не открылась");
+        //5. Закрыть браузер
+        driver.quit();
+    }
+}
