cd3e50fea0ae5894d286e6f1c93993440469ef17
gk <ishkredov2@gmail.com>
2023-06-06 21:11:56 +0300

    Commit

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
 
     WebDriver driver;
@@ -13,9 +15,12 @@ public class BaseTest {
 // а почему именно метод а не тест?
 public void setUp(){
     WebDriverManager.chromedriver().setup();
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--start-maximized");
-    driver = new ChromeDriver(options);
+    driver = new ChromeDriver();
+//    ChromeOptions options = new ChromeOptions();
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//    options.addArguments("--start-maximized");
+//    driver = new ChromeDriver(options);
 
 }
 
