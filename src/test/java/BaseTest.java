c1267773c94fc5c859f489a846a1c49215dec250
gk <ishkredov2@gmail.com>
2023-06-06 20:33:00 +0300

    Commit

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,8 +1,25 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 
     WebDriver driver;
-}
+
 @BeforeMethod
+// нужно закинуть понигарсия или как его там
+public void setUp(){
+    WebDriverManager.chromedriver().setup;
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--start-maximized");
+    driver = new ChromeDriver(options);
+
+}
+
+@AfterMethod(alwaysRun = true)
+public void tearDown(){
+    driver.quit();
+}}
