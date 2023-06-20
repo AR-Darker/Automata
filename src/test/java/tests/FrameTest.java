d95c5f9fdf61659496960d7d6dafa5340134dec7
gk <ishkredov2@gmail.com>
2023-06-20 21:08:51 +0300

    new commit

---
+++ src/test/java/tests/FrameTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class FrameTest extends BaseTest1{
+
+    @Test
+    public void ddddd(){
+        driver.get("herokuappp");
+        drive
+    }
+}
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,5 +1,10 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -38,6 +43,21 @@ public class LoginTest extends BaseTest1 {
         loginPage.open();
         loginPage.login("locked_out_user","secret_sauce");
         assertEquals(loginPage.getErrorMessage(),"Epic sadface: Sorry, this user has been locked out.","текст сообщения неверный");
+    }
+    @Test
+    public void waiter(){
+        loginPage.open();
+        loginPage.login("locked_out_user","secret_sauce");
+        driver.findElement(By.id("react-burger-menu-bnt")).click();
+        driver.findElement(By.id("about-sidebar-link")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("about-sidebar-link")));
+
+
+
+        Actions actions = new Actions(driver);
+        actions.
+
     }
 
 
